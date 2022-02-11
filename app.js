98e86dbf0e9e83ea21272be31df5fe9aebd02df7
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-02-11 14:02:40 +0100

    new push

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express")
-//var favicon = require('serve-favicon')
+var favicon = require('serve-favicon')
 var path = require('path')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
-
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.static('public'))
 
 app.get('/', function(req, res) {
